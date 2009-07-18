699bb88bae31e289e1ba033c41ecd7021f764754
len smith <ignu@lazerbeak.(none)>
2009-07-18 01:14:43 -0400

    updating environment file for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -7,35 +7,25 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
 
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+#################################
+# GEMS NEEDED FOR DEV BUT NOT ON HEROKU:
+##################################
 
-  config.gem "rspec", :lib => false, :version => ">= 1.2.0"
-  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
-  config.gem 'thoughtbot-shoulda', :lib => false, :source=>"http://gems.github.com"
-  config.gem 'haml-edge', :lib => 'haml'
-  config.gem 'chriseppstein-compass', :lib => 'compass', :source=>"http://gems.github.com"
+  #config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+  #config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+  #config.gem 'thoughtbot-shoulda', :lib => false, :source=>"http://gems.github.com"
+  
+  
+  # don't know about these on heroku
+  
+  #config.gem 'haml-edge', :lib => 'haml'  
+  #config.gem 'chriseppstein-compass', :lib => 'compass', :source=>"http://gems.github.com"
   #config.gem 'authlogic', :lib => false
 
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Activate observers that should always be running
+# Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
