7d549e0473000590a9bfaa0679855bced65af4af
Aaron Worsham <aaronworsham@gmail.com>
2009-07-18 14:27:29 -0400

    checking in date params in url all the way to reservation

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ ActionController::Routing::Routes.draw do |map|
     :controller => 'campgrounds',
     :action => 'list'
   }) 
-  map.resources :reservation
-  map.resources :campgrounds, :has_many => :reservations
-  map.list_reservations '/campgrounds/:campground_id/reservations/:year/:month/:day/', :controller => 'reservations', :action => 'index'
-
+  map.resources :reservations 
+  map.resources :campsites
+  map.resources :campgrounds, :has_many => [:reservations, :campsites]
+  map.list_reservations '/campgrounds/:campground_id/reservations/start/:s_year/:s_month/:s_day/end/:e_year/:e_month/:e_day/', :controller => 'reservations', :action => 'index'
+  map.new_reservation '/campsite/:campsite_id/reservation/new/start/:s_year/:s_month/:s_day/end/:e_year/:e_month/:e_day/', :controller => 'reservations', :action => 'new', :conditions => {:method => :post}
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
