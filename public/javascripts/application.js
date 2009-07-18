f7afa33c233b3f4692d4681f32ac15c8a4ba72c7
len smith <ignu@lazerbeak.(none)>
2009-07-18 18:06:43 -0400

    prettied up the js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,32 +8,50 @@ var tehnt = {
        item.find('.date').datepicker();
         $("#ui-datepicker-div").addClass("promoteZ");
        $('.reservation').live('click', function() {
-           tehnt.selectReservationDates();
+           tehnt.selectReservationDates($(this).attr("campground_id"),
+                   $(this).attr("campground_name") );
        });
     }    
 };
 
-tehnt.selectReservationDates = function() {
+
+tehnt.selectReservationDates = function(campground_id, campground_name) {
+    tehnt.currentCampgroundId = campground_id;
     
-    if (!tehnt.selectReservationDates.dialog)
-        tehnt.selectReservationDates.dialog = $('#reservation_dates').dialog({
-            modal:true,
-            title:"Select your camping dates",
-            width:500,
-            buttons: { "Submit" : tehnt.submitReservation},
-            beforeclose:function() {$("#ui-datepicker-div").hide();}
-        });
-    tehnt.selectReservationDates.dialog.dialog('open');
+    var dialog = $('#reservation_dates').dialog({
+        modal:true,
+        width:500,
+        buttons: { "Submit" : tehnt.submitReservation},
+        beforeclose:function() {$("#ui-datepicker-div").hide();}
+    });
+
+    $('.ui-dialog-title').text("Select camping dates for " + campground_name);
+
+    dialog.dialog('open');
 };
 
 tehnt.submitReservation = function() {
+
     var startDate = $('#startDate').datepicker('getDate');
     var endDate = $('#endDate').datepicker('getDate');
 
-    if (!startDate || !endDate)
-      tehnt.showMessage("Please.  Select a Start Date and End Date.");
+    if (!startDate || !endDate) {
+      tehnt.showMessage("Please select a Start Date and End Date.");
+      return false;
+    }
 
-    alert('you selected' + startDate + endDate);
+    if(startDate >= endDate) {
+      tehnt.showMessage("Start date must be before the End Date.");
+      return false;
+    }
+
+    log(startDate.getMonth() + 1);
+
+    log(startDate.getDate());
+    log(startDate.getFullYear());
+
+    
+
 
 };
 
@@ -44,7 +62,7 @@ tehnt.showMessage = function(data) {
     var messageID = "filterArray" + tehnt.messageCount;
     var newMessage = $('<div class="message ui-state-highlight" id="' + messageID + '" name="' + messageID + '">' + data + "</div>");
     $('#flashMessage').append(newMessage);
-    setTimeout("$('#" + messageID + "').fadeOut('fast');", 5000);
+    setTimeout("$('#" + messageID + "').fadeOut('fast');", 4200);
 };
 
 $(function() {
