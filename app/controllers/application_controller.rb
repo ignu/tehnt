2d7c47b9881e765c92a7f2133574fe8f0a8cd7b6
Trek Glowacki <pietrekg@umich.edu>
2009-07-19 03:54:12 -0400

    moved start_date/end_date filters into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,19 @@ class ApplicationController < ActionController::Base
     error_message << "</ul>"
   end
   
+  protected
+    def get_start_date
+      logger.info "#{params[:s_year]}, #{params[:s_month]}, #{params[:s_day]}"
+      @start_date = "#{params[:s_year]}-#{params[:s_month]}-#{params[:s_day]}".to_date
+      logger.info "Start date = #{@start_date}"
+       # Date.new(params[:s_year], params[:s_month], params[:s_day])   
+    end
+  
+    def get_end_date 
+      logger.info "#{params[:e_year]}, #{params[:e_month]}, #{params[:e_day]}"
+      @end_date = "#{params[:e_year]}-#{params[:e_month]}-#{params[:e_day]}".to_date
+      logger.info "End date = #{@end_date}"
+      # @end_date = Date.new(params[:e_year], params[:e_month], params[:e_day])   
+    end
+  
 end
